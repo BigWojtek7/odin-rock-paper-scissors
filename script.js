bdf5f187f63f1a4604977c585630810051045407
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-02-10 18:38:04 +0100

    Add singleRound function
    It decides who win one round

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+
+function singleRound(computerChoice, userChoice) {
+  if (computerChoice === userChoice){
+    return "Tie! You chose the same as computer"
+  } else if (userChoice === "rock"){
+    if (computerChoice === "scissors"){
+      userScore++;
+      return "You Win! Rock beats Scissors"
+    } else if (computerChoice === "paper"){
+      computerScore++;
+      return "You Loose! Paper beats Rock"
+    }
+  } else if (userChoice === "paper"){
+    if (computerChoice === "rock"){
+      userScore++;
+      return "You Win! Paper beats Rock"
+    } else if (computerChoice === "scissors"){
+      computerScore++;
+      return "You Loose! Scissors beats Paper"
+    } 
+  } else if (userChoice === "scissors"){
+    if (computerChoice === "rock"){
+      computerScore++;
+      return "You Loose! Rock beats Scissors"
+    } else if (computerChoice === "paper"){
+      userScore++;
+      return "You Win! Scissors beats Paper"
+    }
+  }
+}
