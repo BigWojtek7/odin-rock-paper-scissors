a40a7821e93e79c1f7466dbd0dfd640785bdca3e
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-02-13 19:13:20 +0100

    Add comments to script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let computerScore = 0;
 let userScore = 0;
 
-
 function getComputerChoice() {
   randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0){
@@ -15,95 +14,80 @@ function getComputerChoice() {
 
 function singleRound(computerChoice, userChoice) {
   if (computerChoice === userChoice){
-    return "Tie! You chose the same as computer"
+    return "Tie! You chose the same as computer."
   } else if (userChoice === "rock"){
     if (computerChoice === "scissors"){
       userScore++;
-      return "You Win! Rock beats Scissors"
+      return "You Win! Rock beats Scissors."
     } else if (computerChoice === "paper"){
       computerScore++;
-      return "You Loose! Paper beats Rock"
+      return "You Loose! Paper beats Rock."
     }
   } else if (userChoice === "paper"){
     if (computerChoice === "rock"){
       userScore++;
-      return "You Win! Paper beats Rock"
+      return "You Win! Paper beats Rock."
     } else if (computerChoice === "scissors"){
       computerScore++;
-      return "You Loose! Scissors beats Paper"
+      return "You Loose! Scissors beats Paper."
     } 
   } else if (userChoice === "scissors"){
     if (computerChoice === "rock"){
       computerScore++;
-      return "You Loose! Rock beats Scissors"
+      return "You Loose! Rock beats Scissors."
     } else if (computerChoice === "paper"){
       userScore++;
-      return "You Win! Scissors beats Paper"
+      return "You Win! Scissors beats Paper."
     }
   }
 }
 
-
-
-
 function playGame(){
-
-
   const container = document.querySelector('.container');
-  
+
   // buttons
   const rockButton = document.createElement('button');
   rockButton.value = "rock";
   rockButton.textContent = "Rock";
+  container.appendChild(rockButton)
 
   const paperButton = document.createElement('button');
   paperButton.value = "paper";
   paperButton.textContent = "Paper";
+  container.appendChild(paperButton)
 
   const scissorsButton = document.createElement('button');
   scissorsButton.value = "scissors";
   scissorsButton.textContent = "Scissors";
-
-
-  
-  
-  const divText = document.createElement('div');
-  
-  
-  
-  container.appendChild(rockButton)
-  container.appendChild(paperButton)
   container.appendChild(scissorsButton)
+
   
-  const divText2 = document.createElement('div');
-  
-  container.appendChild(divText2);
-  container.appendChild(divText);
-  
-  divText.style.fontWeight = 'bold';
+  // Div for text for final result after 5 rounds
+  const divFinalText = document.createElement('div');
+  container.appendChild(divFinalText);
+  divFinalText.style.fontWeight = 'bold';
 
-
-
-
-
-
+  // Div for text for each round score
+  const divRoundText = document.createElement('div');
+  container.appendChild(divRoundText);
 
   let count = 1;
   const buttons = document.querySelectorAll('button');
+
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       let userChoice = button.value;
-      divText2.textContent = `Round ${count++}: 
+      divRoundText.textContent = `Round ${count++}: 
       ${singleRound(getComputerChoice(), userChoice)}
       The Score is: You: ${userScore} Computer: ${computerScore}`;
       if (count > 5){
         buttons.forEach(button => button.disabled = true);
         if (computerScore < userScore){
-          divText.textContent = `You won after 5 rounds.The Score is: You: ${userScore} Computer: ${computerScore}`;
+          divFinalText.textContent = `You won after 5 rounds.The Final Score is: You: ${userScore} Computer: ${computerScore}`;
         } else if (computerScore > userScore){
-          divText.textContent =`You lost after 5 rounds. The Score is: You: ${userScore} Computer: ${computerScore}`;
+          divFinalText.textContent =`You lost after 5 rounds. The Final Score is: You: ${userScore} Computer: ${computerScore}`;
         } else {
-          divText.textContent =`There is a tie after 5 rounds. The Score is: You: ${userScore} Computer: ${computerScore}`;
+          divFinalText.textContent =`There is a tie after 5 rounds. The Final Score is: You: ${userScore} Computer: ${computerScore}`;
         }
       };
     });
